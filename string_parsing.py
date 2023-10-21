5421e9b20974f36172c3af3e82245534a9b65a3a
Rad Warmbold <cradbold@gmail.com>
2023-10-21 16:56:01 -0700

    roman string to int func

---
+++ string_parsing.py
@@ -0,0 +1,73 @@
+class StringParsing:
+
+    @staticmethod
+    def romanToInt(s: str) -> int:
+        sum = 0
+
+        i = 0
+        while (i < len(s)):
+            c = s[i]
+
+            match c:
+                case "I":
+                    if (i + 1 < len(s)):
+                        next = s[i + 1]
+                        if (next == "V"):
+                            sum += 4
+                            i += 2
+                            continue
+                        elif (next == "X"):
+                            sum += 9
+                            i += 2
+                            continue
+                    sum += 1
+                case "V":
+                    sum += 5
+                case "X":
+                    if (i + 1 < len(s)):
+                        next = s[i + 1]
+                        if (next == "L"):
+                            sum += 40
+                            i += 2
+                            continue
+                        elif (next == "C"):
+                            sum += 90
+                            i += 2
+                            continue
+                    sum += 10
+                case "L":
+                    sum += 50
+                case "C":
+                    if (i + 1 < len(s)):
+                        next = s[i + 1]
+                        if (next == "D"):
+                            sum += 400
+                            i += 2
+                            continue
+                        elif (next == "M"):
+                            sum += 900
+                            i += 2
+                            continue
+                    sum += 100
+                case "D":
+                    sum += 500
+                case "M":
+                    sum += 1000
+                case _:
+                    print(f"Unrecognized char: {c}")
+
+            i += 1
+
+        return sum
+
+from typing import Callable, List, Any
+
+def assert_string_parsing(func: Callable, args: List, val: Any) -> None:
+    print(f'Calling {func.__name__} with args: {args} and asserting return value: {val}')
+    result = func(*args)
+    print(f'  Result: {result == val}')
+    assert result == val
+
+assert_string_parsing(StringParsing.romanToInt, ["III"], 3)
+assert_string_parsing(StringParsing.romanToInt, ["LVIII"], 58)
+assert_string_parsing(StringParsing.romanToInt, ["MCMXCIV"], 1994)
