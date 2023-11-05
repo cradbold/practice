eac3652e0910f595794dbf37d03213bb2a954a74
Rad Warmbold <cradbold@gmail.com>
2023-11-05 01:43:05 -0700

    kill comments

--- src/string_parsing.py
+++ src/string_parsing.py
@@ -140,13 +140,6 @@ class StringParsing:
 
         return end_i - start_i
     
-    # Given two binary strings a and b, return their sum as a binary string.
-    # Example 1:
-    # Input: a = "11", b = "1"
-    # Output: "100"
-    # Example 2:
-    # Input: a = "1010", b = "1011"
-    # Output: "10101"
     @staticmethod
     def add_binary_str(a: str, b: str) -> str:
         result = ""
