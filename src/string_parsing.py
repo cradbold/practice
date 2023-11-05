de957ca5c881be15ce07e982bf6d58e48faf2130
Rad Warmbold <cradbold@gmail.com>
2023-11-05 01:42:23 -0700

    high read sol'n

--- src/string_parsing.py
+++ src/string_parsing.py
@@ -149,22 +149,22 @@ class StringParsing:
     # Output: "10101"
     @staticmethod
     def add_binary_str(a: str, b: str) -> str:
-        s = []
+        result = ""
+        
         carry = 0
-        i = len(a) - 1
-        j = len(b) - 1
-
-        while i >= 0 or j >= 0 or carry:
+        i, j = len(a) - 1, len(b) - 1
+        while i >= 0 or j >= 0 or carry != 0:
+            sum = carry
             if i >= 0:
-                carry += int(a[i])
+                sum += int(a[i])
                 i -= 1
             if j >= 0:
-                carry += int(b[j])
+                sum += int(b[j])
                 j -= 1
-            s.append(str(carry % 2))
-            carry //= 2
-
-        return ''.join(reversed(s))
+            carry = sum // 2
+            result = str(sum % 2) + result
+        
+        return result
 
 
 def assert_string_parsing(func: Callable, args: List, val: Any) -> None:
