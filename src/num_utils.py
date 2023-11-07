6591fce564e069bcfa7e8d892f634ad56eb0e4b6
Rad Warmbold <cradbold@gmail.com>
2023-11-07 01:13:47 -0800

    complete impl w/built-in sqrt

--- src/num_utils.py
+++ src/num_utils.py
@@ -1,8 +1,14 @@
+import math
+
 class NumUtils:
 
     @staticmethod
     def my_sqrt(x: int) -> int:
-        return x ** 0.5
+        return math.floor(x ** 0.5)
 
 result = NumUtils.my_sqrt(4)
 assert (result == 2)
+result = NumUtils.my_sqrt(5)
+assert (result == 2)
+result = NumUtils.my_sqrt(2147483647)
+assert (result == 46340)
