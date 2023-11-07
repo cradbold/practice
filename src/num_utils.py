06626b7f0388ac5690bfab729f9a5d7288f06450
Rad Warmbold <cradbold@gmail.com>
2023-11-07 01:41:10 -0800

    added test; brute force impl

--- src/num_utils.py
+++ src/num_utils.py
@@ -4,11 +4,31 @@ class NumUtils:
 
     @staticmethod
     def my_sqrt(x: int) -> int:
-        return floor(x ** 0.5)
 
-result = NumUtils.my_sqrt(4)
+        if (x <= 1):
+            return x
+        
+        for val in range(1, x):
+            sq = val * val
+            if (sq > x):
+                return val - 1
+
+        return -1
+    
+
+x = 4
+result = NumUtils.my_sqrt(x)
+print(f'sqrt({x}) = {result}')
 assert (result == 2)
-result = NumUtils.my_sqrt(5)
+x = 5
+result = NumUtils.my_sqrt(x)
+print(f'sqrt({x}) = {result}')
 assert (result == 2)
-result = NumUtils.my_sqrt(2147483647)
+x = 8
+result = NumUtils.my_sqrt(x)
+print(f'sqrt({x}) = {result}')
+assert (result == 2)
+x = 2147483647
+result = NumUtils.my_sqrt(x)
+print(f'sqrt({x}) = {result}')
 assert (result == 46340)
