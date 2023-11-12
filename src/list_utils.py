71fb961a8b36604ba436a1944781e640645da7ec
Rad Warmbold <cradbold@gmail.com>
2023-11-11 21:19:04 -0800

    style nits

--- src/list_utils.py
+++ src/list_utils.py
@@ -83,8 +83,8 @@ class ListUtils:
     @staticmethod
     def delete_duplicates(head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
-        while curr and curr.next:
-            if curr.val == curr.next.val:
+        while (curr and curr.next):
+            if (curr.val == curr.next.val):
                 curr.next = curr.next.next
             else:
                 curr = curr.next
