9d267912014a03a6249a971e27a4e80e5a30c6ac
Rad Warmbold <cradbold@gmail.com>
2023-11-15 20:05:47 -0800

    add prac prob method sig

--- src/tree_utils.py
+++ src/tree_utils.py
@@ -25,6 +25,15 @@ class TreeUtils:
             root = root.right
         
         return result
+    
+    # Definition for a binary tree node.
+    # class TreeNode:
+    #     def __init__(self, val=0, left=None, right=None):
+    #         self.val = val
+    #         self.left = left
+    #         self.right = right
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        pass
 
 
 tn1 = TreeNode(1)
