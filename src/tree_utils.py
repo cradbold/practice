cb7f800f7e1b332af69e6cbc75911e18d48a09b2
Rad Warmbold <cradbold@gmail.com>
2023-11-16 10:51:26 -0800

    updated func sig w/pass

--- src/tree_utils.py
+++ src/tree_utils.py
@@ -36,16 +36,9 @@ class TreeUtils:
 
         return TreeUtils.is_same_tree(p.left, q.left) and TreeUtils.is_same_tree(p.right, q.right)
     
-    # Definition for a binary tree node.
-    # class TreeNode:
-    #     def __init__(self, val=0, left=None, right=None):
-    #         self.val = val
-    #         self.left = left
-    #         self.right = right
-    class Solution:
-        def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-            pass
-        
+    @staticmethod
+    def is_symmetric(root: Optional[TreeNode]) -> bool:
+        pass
 
 
 tn1 = TreeNode(1)
