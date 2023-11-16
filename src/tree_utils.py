0a925db5b04ae2a595108c7106e40b81ac7acd20
Rad Warmbold <cradbold@gmail.com>
2023-11-15 19:29:25 -0800

    separate tree utils w/tests

---
+++ src/tree_utils.py
@@ -0,0 +1,40 @@
+from typing import Optional, Callable, List, Any
+
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class TreeUtils:
+
+    @staticmethod
+    def in_order_traversal(root: Optional[TreeNode]) -> List[int]:
+        subtree = []
+        result = []
+
+        while (root or subtree):
+            while root:
+                subtree.append(root)
+                root = root.left
+            
+            root = subtree.pop()
+            result.append(root.val)
+            root = root.right
+        
+        return result
+
+
+tn1 = TreeNode(1)
+tn2 = TreeNode(2)
+tn3 = TreeNode(3)
+tn1.right = tn2
+tn2.left = tn3
+result = TreeUtils.in_order_traversal(tn1)
+assert (result == [1, 3, 2])
+result = TreeUtils.in_order_traversal(None)
+assert (result == [])
+result = TreeUtils.in_order_traversal(TreeNode(1))
+assert (result == [1])
