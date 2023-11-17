f247ab1e0cfa958f2d8b9318d4dac4b8d4cca2a2
Rad Warmbold <cradbold@gmail.com>
2023-11-17 11:01:47 -0800

    fix pasc tri test

--- src/list_utils.py
+++ src/list_utils.py
@@ -213,6 +213,6 @@ result = ListUtils.merge([0], 0, [1], 1)
 assert (result == [1])
 
 result = ListUtils.generate_pascal_triangle(5)
-assert (result == [[1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1]])
+assert (result == [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]])
 result = ListUtils.generate_pascal_triangle(1)
 assert (result == [[1]])
