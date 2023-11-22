dd6c3a735eed3b1c386ad18f6c55deb74f98740f
Rad Warmbold <cradbold@gmail.com>
2023-11-22 12:46:00 -0800

    added prac prob placeholder (Stack)

--- src/list_utils.py
+++ src/list_utils.py
@@ -7,6 +7,32 @@ class ListNode:
         self.next = next
 
 
+class Stack:
+
+    def __init__(self):
+        pass
+
+    def push(self, x: int) -> None:
+        pass
+
+    def pop(self) -> int:
+        pass
+
+    def top(self) -> int:
+        pass
+
+    def empty(self) -> bool:
+        pass
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
+
+
 class ListUtils:
 
     @staticmethod
