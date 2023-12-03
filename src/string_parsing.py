7f155dfa70a4cd334d6a7755a585ea6d06f01139
Rad Warmbold <cradbold@gmail.com>
2023-12-03 11:25:19 -0800

    added prac prob f sig

--- src/string_parsing.py
+++ src/string_parsing.py
@@ -327,6 +327,9 @@ class StringParsing:
 
         return nums_with_n_bin_ones
     
+    def longestPalindrome(self, s: str) -> int:
+        pass
+    
 
 def assert_string_parsing(func: Callable, args: List, val: Any) -> None:
     print(f'Calling {func.__name__} with args: {args} and asserting return value: {val}')
