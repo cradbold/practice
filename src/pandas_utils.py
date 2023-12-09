0f10e1688742a60a2aeb14392dc87984d1bf75bb
Rad Warmbold <cradbold@gmail.com>
2023-12-09 14:13:54 -0800

    first impl; test passing

--- src/pandas_utils.py
+++ src/pandas_utils.py
@@ -10,10 +10,10 @@ import pandas as pd
 # | 3         | 4         | 2018-07-03 | 5            |
 # +-----------+-----------+------------+--------------+
 def player_first_login(activity: pd.DataFrame) -> pd.DataFrame:
-    return activity
+    return activity.groupby('player_id').agg({ 'event_date': min }).rename(columns={ 'event_date': 'first_login' }).reset_index()
 
 input_table = { 'player_id': [1, 1, 2, 3, 3], 'device_id':[2, 2, 3, 1, 4], 'event_date': ['2016-03-01', '2016-05-02', '2017-06-25', '2016-03-02', '2018-07-03'], 'games_played': [5, 6, 1, 0, 5] }
-expected_table = { 'player_id': [1, 2, 3], 'first_login': ['2016-03-01', '2017-06-25', '2016-03-02'] }
+expected_table = { 'player_id': { 0: 1, 1: 2, 2: 3 }, 'first_login': { 0: '2016-03-01', 1: '2017-06-25', 2: '2016-03-02' } }
 result_table = player_first_login(pd.DataFrame(input_table)).to_dict()
-print(f'input: {input_table}, expected: {expected_table}, result: {result_table}')
+print(f'input: {input_table}\nexpected: {expected_table}\nresult: {result_table}')
 assert (result_table == expected_table)
