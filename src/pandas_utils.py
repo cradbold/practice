67a7d66f8511978a7d07f01dda5240ea3bc2183e
Rad Warmbold <cradbold@gmail.com>
2023-12-09 13:56:08 -0800

    added test template

--- src/pandas_utils.py
+++ src/pandas_utils.py
@@ -1,4 +1,19 @@
 import pandas as pd
 
-def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+# +-----------+-----------+------------+--------------+
+# | player_id | device_id | event_date | games_played |
+# +-----------+-----------+------------+--------------+
+# | 1         | 2         | 2016-03-01 | 5            |
+# | 1         | 2         | 2016-05-02 | 6            |
+# | 2         | 3         | 2017-06-25 | 1            |
+# | 3         | 1         | 2016-03-02 | 0            |
+# | 3         | 4         | 2018-07-03 | 5            |
+# +-----------+-----------+------------+--------------+
+def player_first_login(activity: pd.DataFrame) -> pd.DataFrame:
     pass
+
+input_table = None
+expected_table = None
+result_table = None
+print(f'input: {input_table}, expected: {expected_table}, result: {result_table}')
+assert (result_table = expected_table)
