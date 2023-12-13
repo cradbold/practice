6521d4e7c1ecd0ff694806c8717d00c648164334
Rad Warmbold <cradbold@gmail.com>
2023-12-12 16:10:30 -0800

    added prac prob

--- src/pandas_utils.py
+++ src/pandas_utils.py
@@ -12,6 +12,26 @@ import pandas as pd
 def player_first_login(activity: pd.DataFrame) -> pd.DataFrame:
     return activity.groupby('player_id').agg({ 'event_date': min }).rename(columns={ 'event_date': 'first_login' }).reset_index()
 
+# Employee
+# +-------------+---------+
+# | Column Name | Type    |
+# +-------------+---------+
+# | empId       | int     |
+# | name        | varchar |
+# | supervisor  | int     |
+# | salary      | int     |
+# +-------------+---------+
+# Bonus
+# +-------------+------+
+# | Column Name | Type |
+# +-------------+------+
+# | empId       | int  |
+# | bonus       | int  |
+# +-------------+------+
+def low_bonus_employees(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+    return bonus
+
+
 input_table = { 'player_id': [1, 1, 2, 3, 3], 'device_id':[2, 2, 3, 1, 4], 'event_date': ['2016-03-01', '2016-05-02', '2017-06-25', '2016-03-02', '2018-07-03'], 'games_played': [5, 6, 1, 0, 5] }
 expected_table = { 'player_id': { 0: 1, 1: 2, 2: 3 }, 'first_login': { 0: '2016-03-01', 1: '2017-06-25', 2: '2016-03-02' } }
 result_table = player_first_login(pd.DataFrame(input_table)).to_dict()
