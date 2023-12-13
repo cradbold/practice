4bee5d5cb1352a196dd7f735a79581834d5c5e39
Rad Warmbold <cradbold@gmail.com>
2023-12-12 17:46:07 -0800

    first impl; tests passing...

--- src/pandas_utils.py
+++ src/pandas_utils.py
@@ -28,8 +28,13 @@ def player_first_login(activity: pd.DataFrame) -> pd.DataFrame:
 # | empId       | int  |
 # | bonus       | int  |
 # +-------------+------+
-def low_bonus_employees(employee: pd.DataFrame, bonus: pd.DataFrame, exc_limit: int) -> pd.DataFrame:
-    pass
+def low_bonus_employees(employees: pd.DataFrame, bonuses: pd.DataFrame, exc_limit: int) -> pd.DataFrame:
+    employees_bonuses = pd.merge(employees, bonuses, how='left', on='empId')
+    employees_bonuses = employees_bonuses.fillna(0)
+    employees_bonuses['bonus'] = employees_bonuses['bonus'].astype(int)
+    result = employees_bonuses[employees_bonuses['bonus'] < exc_limit]
+    result.loc[:'bonus'] = result.loc[:'bonus'].replace(0, None)
+    return result[['name', 'bonus']]
 
 
 input_table = { 'player_id': [1, 1, 2, 3, 3], 'device_id':[2, 2, 3, 1, 4], 'event_date': ['2016-03-01', '2016-05-02', '2017-06-25', '2016-03-02', '2018-07-03'], 'games_played': [5, 6, 1, 0, 5] }
@@ -40,8 +45,8 @@ assert (result_table == expected_table)
 print()
 
 employees = { 'empId': [3, 1, 2, 4], 'name':['Brad', 'John', 'Dan', 'Thomas'], 'supervisor': [None, 3, 3, 3], 'salary': [4000, 1000, 2000, 4000] }
-bonuses = { 'empId': [2, 4], 'bonus': [500, 200] }
-expected = { 'name': { 0: 'Brad', 1: 'John', 2: 'Dan' }, 'bonus': { 0: None, 1: None, 2: '500' } }
+bonuses = { 'empId': [2, 4], 'bonus': [500, 2000] }
+expected = { 'name': { 0: 'Brad', 1: 'John', 2: 'Dan' }, 'bonus': { 0: None, 1: None, 2: 500 } }
 result = low_bonus_employees(pd.DataFrame(employees), pd.DataFrame(bonuses), 1000).to_dict()
 print(f'employees: {employees}\nbonuses: {bonuses}\nexpected: {expected}\nresult: {result}')
 assert (result == expected)
