82c01d19b45488198e379849f6fa907e52106ca2
Rad Warmbold <cradbold@gmail.com>
2023-12-14 13:35:19 -0800

    added prac prob

--- src/pandas_utils.py
+++ src/pandas_utils.py
@@ -45,6 +45,15 @@ def low_bonus_employees(employees: pd.DataFrame, bonuses: pd.DataFrame, exc_limi
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     return world[['name', 'population', 'area']][(world.population >= 25_000_000) | (world.area >= 3_000_000)]
 
+# +-------------+---------+
+# | Column Name | Type    |
+# +-------------+---------+
+# | student     | varchar |
+# | class       | varchar |
+# +-------------+---------+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    pass
+
 
 input_table = { 'player_id': [1, 1, 2, 3, 3], 'device_id':[2, 2, 3, 1, 4], 'event_date': ['2016-03-01', '2016-05-02', '2017-06-25', '2016-03-02', '2018-07-03'], 'games_played': [5, 6, 1, 0, 5] }
 expected_table = { 'player_id': { 0: 1, 1: 2, 2: 3 }, 'first_login': { 0: '2016-03-01', 1: '2017-06-25', 2: '2016-03-02' } }
