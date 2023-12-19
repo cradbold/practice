6f1aefc28ae6b499c3c66c14499bb98d8706c6c3
Rad Warmbold <cradbold@gmail.com>
2023-12-18 16:34:33 -0800

    updated prev prob f name for clarification

--- src/string_parsing.py
+++ src/string_parsing.py
@@ -329,7 +329,7 @@ class StringParsing:
         return nums_with_n_bin_ones
     
     @staticmethod
-    def longest_palindrome_len(s: str) -> int:
+    def longest_palindrome_seq_len(s: str) -> int:
         long_palin_len = 0
         letter_counts = {}
 
@@ -494,8 +494,8 @@ assert_string_parsing(StringParsing.read_binary_watch, [1], ["0:01", "0:02", "0:
 assert_string_parsing(StringParsing.read_binary_watch, [9], [])
 assert_string_parsing(StringParsing.read_binary_watch, [2], ['0:03', '0:05', '0:06', '0:09', '0:10', '0:12', '0:17', '0:18', '0:20', '0:24', '0:33', '0:34', '0:36', '0:40', '0:48', '1:01', '1:02', '1:04', '1:08', '1:16', '1:32', '2:01', '2:02', '2:04', '2:08', '2:16', '2:32', '3:00', '4:01', '4:02', '4:04', '4:08', '4:16', '4:32', '5:00', '6:00', '8:01', '8:02', '8:04', '8:08', '8:16', '8:32', '9:00', '10:00'])
 
-assert_string_parsing(StringParsing.longest_palindrome_len, ["abccccdd"], 7)
-assert_string_parsing(StringParsing.longest_palindrome_len, ["a"], 1)
+assert_string_parsing(StringParsing.longest_palindrome_seq_len, ["abccccdd"], 7)
+assert_string_parsing(StringParsing.longest_palindrome_seq_len, ["a"], 1)
 
 assert_string_parsing(StringParsing.add_int_strings, ["11", "123"], "134")
 assert_string_parsing(StringParsing.add_int_strings, ["456", "77"], "533")
