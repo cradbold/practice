79589c8c1fc7a054d97cc9e6f225f9206ac10a07
Rad Warmbold <cradbold@gmail.com>
2023-12-22 23:13:07 -0800

    first impl; tests passing; P90

--- src/num_utils.py
+++ src/num_utils.py
@@ -236,7 +236,15 @@ class NumUtils:
         
     @staticmethod
     def to_roman_numeral(num: int) -> str:
-        pass
+        result = ''
+
+        mappings = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+        for mapping in mappings:
+            (divisor, numeral) = mapping
+            result += (num // divisor) * numeral
+            num %= divisor
+
+        return result
 
 
 x = 0
